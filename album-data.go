5dead653a63a3dabaa79be6e6d5ca681812a1760
jaosito1 <josuealdana1@gmail.com>
2026-08-19 02:11:56 -0600

    separate AlbumData struct to its own file

---
+++ album-data.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"net/url"
+	"strconv"
+	"strings"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+type AlbumData struct {
+	Title  string
+	Artist string
+	Year   int
+
+	SpotifyUrl *url.URL
+	YoutubeUrl *url.URL
+	AppleUrl   *url.URL
+
+	Genres []string
+}
+
+func getAlbumData(n *html.Node) AlbumData {
+	var album AlbumData
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.ElementNode {
+			switch getAttribute(c, "class") {
+			case "album-title":
+				album.Title = getTextValue(c)
+
+			case "album-artist":
+				albumNode := findByTag(c, atom.A)
+				album.Artist = getTextValue(albumNode)
+
+			case "album-year":
+				yearNode := findByTag(c, atom.A)
+				val, err := strconv.ParseInt(getTextValue(yearNode), 10, 0)
+
+				if err == nil {
+					album.Year = int(val)
+				}
+
+			case "streaming-links":
+				for v := range c.Descendants() {
+					if v.Type == html.ElementNode && v.DataAtom == atom.A {
+						raw := getAttribute(v, "href")
+
+						u, err := url.Parse(raw)
+						if err != nil {
+							continue
+						}
+
+						if u.Hostname() == "" && strings.Contains(u.String(), "spotify") {
+							album.SpotifyUrl = u
+						}
+
+						if u.Hostname() == "www.youtube.com" {
+							album.YoutubeUrl = u
+						}
+
+						if u.Hostname() == "music.apple.com" {
+							album.AppleUrl = u
+						}
+					}
+				}
+
+			case "album-metadata":
+				var genres []string
+
+				spanNode := findByTag(c, atom.Span)
+				for v := range spanNode.Descendants() {
+					if v.Type == html.ElementNode && v.DataAtom == atom.A {
+						genres = append(genres, getTextValue(v))
+					}
+				}
+
+				album.Genres = genres
+			}
+		}
+	}
+
+	return album
+}
--- main.go
+++ main.go
@@ -9,87 +9,76 @@ import (
 	"golang.org/x/net/html/atom"
 )
 
-const URL_BASE = "https://1001albumsgenerator.com"
+const BASE_URL = "https://1001albumsgenerator.com"
 
 func parseAlbumUrls(doc *html.Node) []string {
 	var res []string
 
-	var tableBody *html.Node
-	for node := range doc.Descendants() {
-		if node.Type == html.ElementNode && node.DataAtom == atom.Tbody {
-			tableBody = node
-			break
-		}
-	}
+	tableBody := findByTag(doc, atom.Tbody)
 
 	var tdList []*html.Node
-	for row := range tableBody.Descendants() {
-		// Iterate over every tr in TableBody
-		if row.Type == html.ElementNode && row.DataAtom == atom.Tr {
-			// Look for the first td and save it
-			for field := range row.Descendants() {
-				if field.Type == html.ElementNode && field.DataAtom == atom.Td {
-					tdList = append(tdList, field)
-					break
-				}
-			}
+	for n := range tableBody.Descendants() {
+		if isTag(n, atom.Tr) {
+			td := findByTag(n, atom.Td)
+			tdList = append(tdList, td)
 		}
 	}
 
-	// Iterate over every td and look for the <a> element
 	for _, td := range tdList {
-		for inner := range td.Descendants() {
-			if inner.Type == html.ElementNode && inner.DataAtom == atom.A {
-				// Look in the attributes to find the href
-				for _, n := range inner.Attr {
-					if n.Key == "href" {
-						res = append(res, n.Val)
-						break
-						// Exit out of the loop once href is found
-					}
-				}
-			}
-		}
+		a := findByTag(td, atom.A)
+		href := getAttribute(a, "href")
+
+		res = append(res, href)
 	}
 
 	return res
 }
 
-type Queue struct {
-	urls []string
+
+func fetchAndParse(url string) (*html.Node, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, nil
 }
 
 func fetchAlbumData(url string) {
-	// resp, err := http.Get("URL_BASE/")
-	// get item from queue
-	// fetch information
-	// send it back via channel
+	doc, err := fetchAndParse(BASE_URL + url)
+	if err != nil {
+		// ignore error or handle it (?)
+	}
+
+	node := findByClass(doc, "album-info")
+	if node == nil {
+		log.Fatal("missing album-info node")
+	}
+
+	data := getAlbumData(node)
+	fmt.Printf("RETURNED ALBUM: %+v", data)
 }
 
 func main() {
 	fmt.Println("Obtaining site data...")
-	resp, err := http.Get(URL_BASE + "/albums")
+
+	doc, err := fetchAndParse(BASE_URL + "/albums")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
 
 	fmt.Println("Parsing urls...")
-	doc, err := html.Parse(resp.Body)
-	if err != nil {
-		log.Fatal("failed to parse html body", err)
-	}
 
 	urls := parseAlbumUrls(doc)
 	if len(urls) <= 0 {
 		log.Fatal("empty url slice")
 	}
-	fmt.Printf("Found %v album urls\n", len(urls))
 
-	q := Queue{urls: urls}
-	fmt.Println(q.urls)
-
-	// for _, url := range q.urls {
-	// 	go fetchAlbumData(url)
-	// }
+	fmt.Printf("Found %v album urls!\n", len(urls))
 }
